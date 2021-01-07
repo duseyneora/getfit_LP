42c8b15a922f5f2a29e2457eafcbdc19f67a2590
duseyneora <dusey@neora.com>
2021-01-07 15:28:09 -0600

    updated v

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+setInterval(() => {
+    plusSlides(1)
+}, 5000)
+
+
+var slideIndex = 1;
+setTimeout(function(){ showSlides(slideIndex); }, 100)
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "flex";
+}
